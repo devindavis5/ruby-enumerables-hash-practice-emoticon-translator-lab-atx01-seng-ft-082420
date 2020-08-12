3c9e8d86e753daef4ae3deb772faba3a405e28d7
Devin Davis <devinrdavis555@gmail.com>
2020-08-12 19:27:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pry'
 def load_library(yaml_file)
   library = YAML.load_file('./lib/emoticons.yml')
   
-final results = library.each_with_object({}) do |item, emoticons_hash|
+final results = library.each_with_object({}) do |(translation, emojis), emoticons_hash|
   
 binding.pry
 end
